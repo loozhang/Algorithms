5a879c1745d0aaa69c01e784ea55f543fc72165e
loozhang <loozhang@163.com>
2017-12-01 23:03:18 +0800

    QuickSort

--- src/com/namaste/sort/QuickSort.java
+++ src/com/namaste/sort/QuickSort.java
@@ -6,31 +6,30 @@ public class QuickSort {
 	public static void main(String[] args) 
 	{
 		int[] arr=new int[] {3,2,1,7,8,5,3};
-		QuickSort(arr,0,arr.length-1);
+		Common.Print(QuickSort(arr,0,arr.length-1));
 		
 	}
 	
-	public static void QuickSort(int[] arr,int l,int r) {
+	public static int[] QuickSort(int[] arr,int l,int r) {
 		if(l>=r) {
-			System.out.println(arr[l]);
-			return;	
 		}
-
-		int i=l;int j=r;int key=arr[l];
-		while(i<j) {
-			while(i<j&&arr[j]>=key)
-				j--;
-			if(i<j)
-				arr[i++]=arr[j];
-			while(i<j&&arr[i]<key)
-				i++;
-			if(i<j)
-				arr[j--]=arr[i];
+		else {
+			int i=l;int j=r;int key=arr[l];
+			while(i<j) {
+				while(i<j&&arr[j]>=key)
+					j--;
+				if(i<j)
+					arr[i++]=arr[j];
+				while(i<j&&arr[i]<key)
+					i++;
+				if(i<j)
+					arr[j--]=arr[i];
+			}
+			arr[i]=key;
+			QuickSort(arr,l,i-1);
+			QuickSort(arr,i+1,r);
 		}
-		arr[i]=key;
-		QuickSort(arr,l,i-1);
-		QuickSort(arr,i+1,r);
 		
-		//Common.Print(arr);
+		return arr;
 	}
 }
