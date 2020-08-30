9541905cf23f0f4ee1ee669587b42a145be32aee
张陆 <zhanglu70@meituan.com>
2020-08-30 22:36:42 +0800

    update

---
+++ src/com/namaste/array/Fibonacci.java
@@ -0,0 +1,19 @@
+package com.namaste.array;
+
+import com.namaste.common.Common;
+
+public class Fibonacci {
+    public static void main(String args[]){
+        int n=10;
+        int[] arr=new int[10];
+        for(int i=0;i<n;i++){
+            arr[i]=func(i);
+        }
+        Common.Print(arr);
+    }
+
+    public static int func(int n){
+        if(n<2) return 1;
+        return func(n-2)+func(n-1);
+    }
+}
