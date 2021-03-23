a512ece0cfc5d43b91613eab233fa89e6534729b
loozhang <loozhang@163.com>
2021-03-23 23:55:57 +0800

    update

--- src/com/namaste/array/IntersectTwoSortedArrays.java
+++ src/com/namaste/array/IntersectTwoSortedArrays.java
@@ -3,6 +3,8 @@ package com.namaste.array;
 import java.util.ArrayList;
 import java.util.List;
 
+//https://blog.csdn.net/sinat_20177327/article/details/80100081
+//https://blog.csdn.net/qq_41900081/article/details/86531432
 public class IntersectTwoSortedArrays {
     public static List intersect(int[] a, int[] b){
         int i=0,j=0,m=a.length,n=b.length;
