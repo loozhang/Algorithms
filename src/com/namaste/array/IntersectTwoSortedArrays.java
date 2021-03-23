c60634970235ab5431b9e7da6d57e3a5b6f2eb64
loozhang <loozhang@163.com>
2021-03-23 23:12:40 +0800

    update

---
+++ src/com/namaste/array/IntersectTwoSortedArrays.java
@@ -0,0 +1,29 @@
+package com.namaste.array;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class IntersectTwoSortedArrays {
+    public static List intersect(int[] a, int[] b){
+        int i=0,j=0,m=a.length,n=b.length;
+        List<Integer> res=new ArrayList<>();
+        while (i<m&&j<n){
+            if(a[i]==b[j]){
+                res.add(a[i]);
+                i++;
+                j++;
+            }else if(a[i]>b[j]){
+                j++;
+            }else {
+                i++;
+            }
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        int a[] = {2,5,6,8,9};
+        int b[] = {1,5,6,7,9};
+        System.out.println(intersect(a,b));
+    }
+}
